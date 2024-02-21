0f94c9831fd0672e7686554897e76b76805f2d82
Miro Stauder <miro@proxysql.com>
2024-02-21 09:38:33 +0000

    various changes/fixes to the bash script

--- connect_speed.bash
+++ connect_speed.bash
@@ -6,7 +6,9 @@
 
 ####################################################################################################
 
-PORT='$(expr $OFFSET + 6033)'
+DB='mysql57'
+OFFSET=0
+PORT=$(expr $OFFSET + 6033)
 
 fn_multiport () {
 	local MPORT=""
@@ -16,28 +18,39 @@ fn_multiport () {
 	echo "${MPORT::-1}"
 }
 
+fn_mquery () {
+	mysql --default-auth=mysql_native_password -uroot -proot -h127.0.0.1 -P3306 -e "${1}" 2>&1 | grep -vP "mysql: .?Warning"
+}
+
+fn_pquery () {
+	mysql --default-auth=mysql_native_password -uadmin -padmin -h127.0.0.1 -P6032 -e "${1}" 2>&1 | grep -vP "mysql: .?Warning"
+}
+
 fn_build () {
 	OFFSET=0
 	for VER in ${VERS}; do
 
-		let OFFSET+=10000
+#		let OFFSET+=10000
 
 		#rm -rf proxysql.${VER} || true
 		echo "============================================================================================="
+		echo "ProxySQL '${VER}'"
 		if [[ ! -d proxysql.${VER} ]]; then
-			echo "Checking out $VER"
+			echo "Cloning repo ..."
 			git clone https://github.com/sysown/proxysql.git proxysql.${VER}
 			git -C proxysql.${VER} checkout ${VER} 2> /dev/null
 		fi
 
 		pushd proxysql.${VER} &> /dev/null
-		if [[ ! -f proxysql ]]; then
-			echo "Building ${VER} ..."
+		if [[ ! -f src/proxysql ]]; then
+			echo "Building ..."
 			make -j$(nproc) &> /dev/null
 		fi
 
-		sed -ir "0,/threads=[0-9]\+/{s//threads=$(expr $NCPUS '/' $MULTI)/}" ./src/proxysql.cfg
-		#sed -ir "0,/threads=[0-9]\+/{s//threads=$NCPUS" ./src/proxysql.cfg
+		echo "Configuring ..."
+		#sed -ir "0,/threads=[0-9]\+/{s//threads=$(expr $NCPUS '/' $MULTI)/}" ./src/proxysql.cfg
+		#sed -ir "0,/threads=[0-9]\+/{s//threads=$(expr 1 '*' $MULTI)/}" ./src/proxysql.cfg
+		sed -ir "0,/threads=[0-9]\+/{s//threads=1/}" ./src/proxysql.cfg
 		grep 'threads=' src/proxysql.cfg | head -1
 		sed -ir "0,/max_connections=[0-9]\+/{s//max_connections=65536/}" ./src/proxysql.cfg
 		grep 'max_connections=' src/proxysql.cfg | head -1
@@ -49,15 +62,34 @@ fn_build () {
 		rm -f ./src/*.db
 
 		#./src/proxysql --idle-threads --sqlite3-server -f -c ./src/proxysql.cfg -D ./src &
-		./src/proxysql -f -c ./src/proxysql.cfg -D ./src  &> /dev/null &
-		sleep 5
+		#./src/proxysql -f -c ./src/proxysql.cfg -D ./src  &> /dev/null &
+		#sleep 5
 
-		mysql --default-auth=mysql_native_password -u admin -padmin -h 127.0.0.1 -P $(expr $OFFSET + 6032) -e "INSERT INTO mysql_users (username,password) VALUES ('sbtest','sbtest'); LOAD mysql users TO RUNTIME; SAVE mysql users TO DISK;"
-#		mysql --default-auth=mysql_native_password -u admin -padmin -h 127.0.0.1 -P $(expr $OFFSET + 6032) -e "SET mysql-interfaces = '0.0.0.0:$(expr $OFFSET + 6033),0.0.0.0:$(expr $OFFSET + 6034),0.0.0.0:$(expr $OFFSET + 6035),0.0.0.0:$(expr $OFFSET + 6036)';"
-		mysql --default-auth=mysql_native_password -u admin -padmin -h 127.0.0.1 -P $(expr $OFFSET + 6032) -e "SELECT @@version;" -E
+		#mysql --default-auth=mysql_native_password -u admin -padmin -h 127.0.0.1 -P $(expr $OFFSET + 6032) -e "INSERT INTO mysql_users (username,password) VALUES ('sbtest','sbtest'); LOAD mysql users TO RUNTIME; SAVE mysql users TO DISK;" 2>&1 | grep -vP "mysql: .?Warning"
+		#mysql --default-auth=mysql_native_password -u admin -padmin -h 127.0.0.1 -P $(expr $OFFSET + 6032) -e "SET mysql-interfaces = '0.0.0.0:$(expr $OFFSET + 6033),0.0.0.0:$(expr $OFFSET + 6034),0.0.0.0:$(expr $OFFSET + 6035),0.0.0.0:$(expr $OFFSET + 6036)';" 2>&1 | grep -vP "mysql: .?Warning"
+		#mysql --default-auth=mysql_native_password -u admin -padmin -h 127.0.0.1 -P $(expr $OFFSET + 6032) -e "SELECT @@version;" -E 2>&1 | grep -vP "mysql: .?Warning" | grep version
 
 		popd &> /dev/null
 	done
+}
+
+fn_gitver() {
+	mysql --default-auth=mysql_native_password -u admin -padmin -h 127.0.0.1 -P $(expr $OFFSET + 6032) -e "SELECT @@version;" -E 2>&1 | grep -vP "mysql: .?Warning" | grep version
+}
+
+fn_waitstate() {
+	MAXWAIT=$@
+#	echo "============================================================================================="
+	WAIT=$(netstat -ntpa | grep WAIT | wc -l)
+	echo -n "Waiting for $WAIT connections to close ..."
+	while [[ $WAIT -ge 100 ]]; do
+		echo -n "."
+		sleep 1
+		WAIT=$(netstat -ntpa | grep WAIT | wc -l)
+	done
+	echo
+	echo "Continuing with $WAIT connections in WAIT state ..."
+	#sleep 60
 }
 
 fn_benchmark () {
@@ -65,30 +97,45 @@ fn_benchmark () {
 	for VER in ${VERS}; do
 
 		SUMM=0
-		let OFFSET+=10000
+#		let OFFSET+=10000
 
 		#CMD="./connect_speed -i 0 -c 1000 -u sbtest -p sbtest -h 127.0.0.1 -P $(expr $OFFSET + 6033) -t 10 -q 0"
 		CMD=$(eval echo "${1}")
 
-		echo "============================================================================================="
-		WAIT=$(netstat -ntpa | grep WAIT | wc -l)
-		while [[ $WAIT -ge 100 ]]; do
-			echo "Waiting 10s for $WAIT connections to close ..."
-			sleep 10
-			WAIT=$(netstat -ntpa | grep WAIT | wc -l)
-		done
-		echo "Continuing with $WAIT connections in WAIT state ..."
+		echo "Starting proxysql $VER ..."
+		killall proxysql
+#		docker-compose -p connect_speed down
 		sleep 60
+#		./proxysql.${VER}/src/proxysql -f -c ./proxysql.${VER}/src/proxysql.cfg -D ./proxysql.${VER}/src &> /dev/null &
+		pushd proxysql.${VER}/src &> /dev/null
+		./proxysql -f -D . &> /dev/null &
+		popd &> /dev/null
 
-		echo "Benchmark ${VER} on port $(expr $OFFSET + 6033) ..."
-		echo "CMD: 'sleep $PAUSE; ${CMD}'"
-		sleep ${PAUSE}
+#		docker-compose -p connect_speed up -d ${DB} && sleep 30
+
+		CNT=$(netstat -ntpl | grep -P ':\d?60[3456789]\d' | uniq -c | grep -v 6032 | wc -l)
+		echo -n "Waiting for $MULTI ports to listen ..."
+		while [[ ! $CNT == $MULTI ]]; do
+			echo -n "."
+			sleep 1
+			CNT=$(netstat -ntpl | grep -P ':\d?60[3456789]\d' | uniq -c | grep -v 6032 | wc -l)
+		done
+		echo
+#		netstat -ntpl | grep -P ':\d60[3456789]\d' | uniq -c
+
+		mysql --default-auth=mysql_native_password -u admin -padmin -h 127.0.0.1 -P $(expr $OFFSET + 6032) -e "INSERT INTO mysql_users (username,password) VALUES ('sbtest','sbtest'); LOAD mysql users TO RUNTIME; SAVE mysql users TO DISK;" 2>&1 | grep -vP "mysql: .?Warning"
+#		mysql --default-auth=mysql_native_password -u admin -padmin -h 127.0.0.1 -P $(expr $OFFSET + 6032) -e "SET admin-admin_credentials='admin:admin;cluster1:secret1pass;sbtest:sbtest'; LOAD admin variables TO RUNTIME; SAVE admin variables TO DISK;" 2>&1 | grep -vP "mysql: .?Warning"
+#		fn_pquery "INSERT INTO mysql_users (username,password,fast_forward) VALUES ('sbtest','sbtest',0); LOAD MYSQL USERS TO RUNTIME;"
+#		fn_pquery "DELETE FROM mysql_servers; INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_connections) VALUES (0,'testinfra-${DB}',3306,1000); LOAD MYSQL SERVERS TO RUNTIME;"
+		echo "Benchmark $(fn_gitver) on port $(expr $OFFSET + 6033)+ ..."
+		echo "CMD: '${CMD}'"
 
 		for N in $(seq $LOOP); do
 
 			#grep </proc/net/tcp -c '^ *[0-9]\+: [0-9A-F: ]\{27\} 01 '
 			#cat /proc/net/softnet_stat | awk '{print $2}' | grep -v 00000000
 
+			fn_waitstate 100
 			LOG=$($CMD 2>&1 | uniq -c)
 
 #			netstat -ntpa | grep -i WAIT
@@ -108,9 +155,14 @@ fn_benchmark () {
 		CPS=$(expr $TOTAL '*' 1000 '/' $SUMM)
 		echo "SUMMARY: ${LOOP} runs, total $TOTAL connections, in ${SUMM}ms, at $CPS conn/s"
 
-	done
+#		docker-compose -p connect_speed down
+		while [[ $(netstat -ntpl) =~ proxysql ]]; do 
+			killall proxysql
+			sleep ${PAUSE}
+		done
 
-	echo "============================================================================================="
+		echo "============================================================================================="
+	done
 }
 
 fn_sysstats () {
@@ -128,13 +180,13 @@ fn_sysstats () {
 	echo "============================================================================================="
 	CNT=$(netstat -ntpl | grep -P ':\d60[3456789]\d' | uniq -c | grep -v 6032 | wc -l)
 	PRT=$(expr $MULTI '*' $(echo $VERS | wc -w))
-	while [[ ! $CNT == $PRT ]]; do
-		echo "Waiting 10s for all ports to listen ..."
-		sleep 10
-		CNT=$(netstat -ntpl | grep -P ':\d60[3456789]\d' | uniq -c | grep -v 6032 | wc -l)
-	done
-	echo "Continuing with $(echo $VERS | wc -w) * $MULTI ports listening ..."
-	netstat -ntpl | grep -P ':\d60[3456789]\d' | uniq -c
+#	while [[ ! $CNT == $PRT ]]; do
+#		echo "Waiting 10s for all ports to listen ..."
+#		sleep 10
+#		CNT=$(netstat -ntpl | grep -P ':\d60[3456789]\d' | uniq -c | grep -v 6032 | wc -l)
+#	done
+#	echo "Continuing with $(echo $VERS | wc -w) * $MULTI ports listening ..."
+#	netstat -ntpl | grep -P ':\d60[3456789]\d' | uniq -c
 
 }
 
@@ -142,10 +194,12 @@ fn_sysstats () {
 # RUN
 ####################################################################################################
 
-NCPUS=64
-MULTI=8
-VERS="v2.3.2 v1.4.16"
-#VERS="v2.0.17"
+NCPUS=32
+MULTI=1
+VERS="v1.4.16 v2.0.17 v2.3.2 v2.5.5 v2.x v2.6.0-update_to_openssl_v3.1.5 v2.6.0-update_to_openssl_v3.2.1"
+#VERS="v2.6.0-update_to_openssl_v3.1.5 v2.6.0-update_to_openssl_v3.2.1"
+#VERS="v2.x v2.5.5 v2.3.2 v2.0.17 v1.4.16"
+#VERS="v2.3.2"
 LOOP=3
 PAUSE=10
 
@@ -156,7 +210,11 @@ fn_build
 
 fn_sysstats
 
-#fn_benchmark "./connect_speed -i 0 -c 100 -u sbtest -p sbtest -h 127.0.0.1 -P $PORT -M $MULTI -t 1 -q 1"
-fn_benchmark "./connect_speed -i 0 -c 10000 -u sbtest -p sbtest -h 127.0.0.1 -P $PORT -M $MULTI -t 128 -q 0"
+fn_benchmark "./connect_speed -i 0 -c 1000 -u sbtest -p sbtest -h 127.0.0.1 -P $PORT -M $MULTI -t 32 -q 0"
+#fn_benchmark "./connect_speed -i 0 -c 1000 -u sbtest -p sbtest -h 127.0.0.1 -S required -P $PORT -M $MULTI -t 32 -q 0"
 
-#killall proxysql
+# test admmin port with query
+#fn_benchmark "./connect_speed -i 0 -c 1000 -u admin -p admin -h 127.0.0.1 -P 6032 -M 1 -t 32 -q 1"
+#fn_benchmark "./connect_speed -i 0 -c 1000 -u admin -p admin -h 127.0.0.1 -S required -P 6032 -M 1 -t 32 -q 1"
+
+killall proxysql
